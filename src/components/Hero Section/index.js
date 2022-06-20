8283604b37ed2a7404026484ceabbaac2f45d229
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-20 21:20:47 +0300

    update photos

--- src/components/Hero Section/index.js
+++ src/components/Hero Section/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import CountriesPhoneCode from './phones.json';
+import CountriesPhoneCode from './phonesCode.json';
 
 import ReactFlagsSelect from "react-flags-select";
 
@@ -29,10 +29,16 @@ export default function HeroSection() {
     <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
       <Slider {...settings}>
       <div>
-        <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="" />
+        <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="getir-photo-1" />
       </div>
       <div>
-        <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-2-7c23764275cdaf14d7b6cf15ebbdd0c1.jpg" alt="" />
+        <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-2-7c23764275cdaf14d7b6cf15ebbdd0c1.jpg" alt="getir-photo-2" />
+      </div>
+      <div>
+        <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-3-ffd1e98c5da51809f9adcc3a4a37d751.jpg" alt="getir-photo-3" />
+      </div>
+      <div>
+        <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" alt="getir-photo-4" />
       </div>
       </Slider>
       <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
@@ -53,8 +59,8 @@ export default function HeroSection() {
                     onSelect={code => setSelected(code)}
                     className="flag-select"
                 />
-                <label className="flex-1 relative block">
-                  <input required className="h-14 px-4 border-2 border-gray-200 rounded w-full transition-colors hover:border-primary-brand-color focus:border-primary-brand-color outline-none peer text-sm pt-2" />
+                <label className="flex-1 relative group block cursor-pointer">
+                  <input required className="h-14 px-4 border-2 border-gray-200 rounded w-full transition-colors group-hover:border-primary-brand-color focus:border-primary-brand-color outline-none peer text-sm pt-2" />
                   <span className="absolute top-0 left-0 h-full px-4 flex items-center text-sm text-gray-700 peer-focus:h-7 peer-focus:text-primary-brand-color peer-focus:text-xs peer-valid:h-7 peer-valid:text-primary-brand-color peer-valid:text-xs transition-all">Telefon Numarası</span>
                 </label>
               </div>
