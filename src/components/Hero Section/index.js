257fe5511e85113edb11289e81c30cd2a193402a
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-20 20:20:55 +0300

    update hero section and tailwind.config

--- src/components/Hero Section/index.js
+++ src/components/Hero Section/index.js
@@ -14,7 +14,7 @@ export default function HeroSection() {
   };
 
   return (
-    <div>
+    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
       <Slider {...settings}>
       <div>
         <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="" />
@@ -23,6 +23,14 @@ export default function HeroSection() {
         <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-2-7c23764275cdaf14d7b6cf15ebbdd0c1.jpg" alt="" />
       </div>
       </Slider>
+      <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+          <div>
+            <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" alt="" />
+            <h3 className="mt-8 text-4xl font-semibold text-white">
+              Dakikalar içinde <br />kapınızda
+            </h3>
+          </div>
+      </div>
     </div>
   )
 }
