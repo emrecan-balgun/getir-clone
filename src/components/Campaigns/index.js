822886d7afc5a6833c9069ef5c5fafd3d9ef25b8
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-21 16:47:09 +0300

    update banners.json and campaingns

--- src/components/Campaigns/index.js
+++ src/components/Campaigns/index.js
@@ -1,5 +1,42 @@
+import { useState, useEffect } from 'react';
+
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+import Banners from 'api/banners.json';
+
 export default function Campaigns() {
+  const [banners, setBanners] = useState([]);
+
+  useEffect(() => {
+      setBanners(Banners);
+  }, [])
+
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    cssEase: "linear"
+  };
+
   return (
-    <div>Campaigns</div>
+    <div className="container mx-auto">
+      <Slider className="md:-mx-2" {...settings}>
+        {banners.length && banners.map((banner) => (
+            <div key={banner.id}>
+                <picture className="block md:px-2">
+                    <img src={banner.image} className="md:rounded-lg" />
+                </picture>
+            </div>
+        ))}
+      </Slider>
+    </div>
   )
 }
