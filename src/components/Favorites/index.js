28398f792de97bdcff4714c0b2a32e77819b9395
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-21 17:17:40 +0300

    update favorites and add ProductItem

--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -1,6 +1,15 @@
+import { useState, useEffect } from 'react'; 
 import Title from 'components/UI/Title';
+import Products from 'api/products.json';
 
 export default function Favorites() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // istek
+    setProducts(Products);
+  }, [])
+
   return (
     <Title>Favoriler</Title>
   )
---
+++ src/components/UI/ProductItem.js
@@ -0,0 +1,7 @@
+export default function ProductItem({ product: { id, title, image, alt, price} }) {
+  return (
+    <div>
+      {title}
+    </div>
+  )
+}
