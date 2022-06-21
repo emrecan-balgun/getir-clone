68065ef78731df1d350690d0666add043acb64ed
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-21 17:35:38 +0300

    update product item

--- src/components/UI/ProductItem.js
+++ src/components/UI/ProductItem.js
@@ -1,6 +1,11 @@
+import { FiPlus } from 'react-icons/fi';
+
 export default function ProductItem({ product: { id, title, image, alt, price} }) {
   return (
-    <div className="bg-white flex flex-col gap-y-1 items-center text-center text-sm font-semibold p-3">
+    <div className="bg-white flex flex-col gap-y-1 relative items-center text-center text-sm font-semibold p-3">
+      <button className="absolute top-3 right-3 bg-white w-8 h-8 flex items-center justify-center border border-gray-200 rounded-lg text-brand-color shadow-md transition-colors hover:border-brand-color">
+        <FiPlus size={16}/>
+      </button>
       <img src={image} alt={title} />
       <div className="text-brand-color">₺{price}</div>
       <div className="text-gray-900">{title}</div>
