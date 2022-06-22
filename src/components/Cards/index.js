233972d0eee245622ee2433702d39ac8c8633aac
Emrecan Balgün <deathlygrave@gmail.com>
2022-06-22 21:09:36 +0300

    update cards.json and cards

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,9 +1,25 @@
 import CardsData from 'api/cards.json';
+import { useState, useEffect } from 'react';
 
 export default function Cards() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    // istek
+    setCards(CardsData);
+  }, []);
+
   return (
-    <div>
-        Cards
+    <div className="grid grid-cols-3 gap-x-4">
+        {
+          cards.length && cards.map(card => (
+            <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center" key={card.id}>
+              <img className="w-[150px] h-[150px] mb-6" src={card.image} alt={card.title} />
+              <h6 className="font-semibold text-lg text-primary-brand-color">{card.title}</h6>
+              <p className="mt-2 text-sm text-gray-500">{card.description}</p>
+            </div>
+          ))
+        }
     </div>
   )
 }
